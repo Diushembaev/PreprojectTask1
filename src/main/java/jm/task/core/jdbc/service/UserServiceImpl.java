fc790ce53f57c884003356d7c8bd118fa20341a3
Diushembaev <xaomi.25022018@gmail.com>
2020-10-31 12:58:13 +0600

    first commit

---
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,105 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    Connection connection = Util.getConnection();
+    public void createUsersTable() {
+       // String query = "CREATE TABLE Users";
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS Users(" +
+                    "`id` INT NOT NULL AUTO_INCREMENT," +
+                    "name TEXT NOT NULL," +
+                    "lastname TEXT NOT NULL," +
+                    "age INTEGER," +
+                    "PRIMARY KEY (`id`)," +
+                    "UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void dropUsersTable() {
+
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS Users");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        connection.setAutoCommit(false);
+        try {
+            String query = "INSERT INTO Users (name, lastname, age) VALUES (\'" + name + "\', \'" +  lastName + "\', " + age +")";
+            Statement statement = Util.getConnection().createStatement();
+            statement.executeUpdate(query);
+            connection.commit();
+        }  catch (Exception e) {
+            e.printStackTrace();
+            connection.rollback();
+        } finally {
+            connection.setAutoCommit(true);
+        }
+    }
+
+    public void removeUserById(long id) throws SQLException {
+        connection.setAutoCommit(false);
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            statement.executeUpdate("DELETE FROM Users WHERE id = id ");
+            connection.commit();
+        }  catch (Exception e) {
+            e.printStackTrace();
+            connection.rollback();
+        } finally {
+            connection.setAutoCommit(true);
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<User>();
+
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Users");
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+                users.add(user);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return (List<User>) users;
+    }
+
+    public void cleanUsersTable() throws SQLException {
+        connection.setAutoCommit(false);
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            statement.executeUpdate("DELETE FROM Users");
+            connection.commit();
+
+        }  catch (Exception e) {
+            e.printStackTrace();
+            connection.rollback();
+        } finally {
+            connection.setAutoCommit(true);
+        }
+    }
+}
